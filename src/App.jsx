e1defad2d4861d9957e353baaf91dbe9ae39e7b3
szuiderweg <svmzuiderweg@hotmail.com>
2024-07-18 11:31:30 +0200

    initial setup of project

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,26 @@ function App() {
 
   return (
     <>
+      <header>
+        <nav>
+            <a href="/">start</a>
+            <a href="/">tuin</a>
+            <a href="/">plantenkiezer</a>
+            <a href="/">plantencatalogus</a>
+            <a href="/">moodboard</a>
+            <a href="/">accounts beheren</a>
+            <button type="button">account</button>
+            <button type="button">uitloggen</button>
+        </nav>
+      </header>
+
+        <body>
       <h1>FSD Eindopdracht Woehoe!</h1>
+        </body>
+
+        <footer>
+            <p>PlantjesPlanner</p>
+        </footer>
     </>
   )
 }
